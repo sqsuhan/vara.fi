d35070f96d0e84cc56250ae8b8fddc1ed46feafa
sqsuhan <123622131+sqsuhan@users.noreply.github.com>
2026-05-23 00:50:01 +0000

    Deploying to gh-pages from @ sqsuhan/vara.fi@102e7aa399529565b29a2843629139eda070085a 🚀

--- assets/chevron-left-DopWY8Pq.js
+++
@@ -1,8 +0,0 @@
-import{U as o}from"./core-BadKG_fp.js";import"./index-B9X31oxF.js";import"./events-b0fKkqMt.js";import"./index.es-B9SfYJQh.js";import"./index-DIwirM5T.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronLeftSvg};
--- assets/chevron-right-Cos09zuJ.js
+++
@@ -1,8 +0,0 @@
-import{U as o}from"./core-BadKG_fp.js";import"./index-B9X31oxF.js";import"./events-b0fKkqMt.js";import"./index.es-B9SfYJQh.js";import"./index-DIwirM5T.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronRightSvg};
